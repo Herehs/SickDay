2fe05f87991487c3600e4cffcb537eefccae73a6
cqr <titovdimka07@gmail.com>
2026-05-02 19:57:18 +0300

    writing advice component

--- app/src/main/java/com/example/up/presentation/main_screen/components/AdviceList.kt
+++ app/src/main/java/com/example/up/presentation/main_screen/components/AdviceList.kt
@@ -1,5 +1,6 @@
 package com.example.up.presentation.main_screen.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,9 +13,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,16 +36,21 @@ fun AdviceList(
                 modifier = Modifier.padding(top = 12.dp, bottom = 12.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Icon(painter = painterResource(advice.icon), contentDescription = null)
+                Icon(
+                    painter = painterResource(advice.icon),
+                    contentDescription = null
+                )
                 Text(
                     modifier = Modifier.padding(start = 10.dp),
                     text = advice.text,
-                    fontSize = 17.sp,
+                    fontSize = 18.sp,
                     lineHeight = 22.sp,
                     fontFamily = bodyFontFamily,
                     color = text,
                     fontWeight = FontWeight.W400,
-                    letterSpacing = -(0.8).sp
+                    letterSpacing = -(0.8).sp,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
@@ -57,7 +66,7 @@ data class Advice(
 @Preview
 @Composable
 fun AdviceListTest(){
-    val advice = Advice(icon = R.drawable.heart_rate, text = "kakoy-to text kakoy-to text kakoy-to text kakoy-to text kakoy-to text kakoy-to text kakoy-to text kakoy-to text kakoy-to text kakoy-to text ")
+    val advice = Advice(icon = R.drawable.heart_rate, text = "hjhhg jkhbhjkghg khgbhgjhj kjjkkhghjftj fhkjhjgh mnftihg")
     val adviceList = mutableListOf<Advice>()
 
     repeat(20){
