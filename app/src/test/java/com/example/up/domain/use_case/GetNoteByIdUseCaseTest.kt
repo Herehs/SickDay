c5047cb24a14782024d9afb5128da3e113d8b4d3
Herehs <titovdimka07@gmail.com>
2026-08-28 14:34:13 +0300

    test: add unit test for GetNoteByIdUseCase

--- app/src/test/java/com/example/up/domain/use_case/GetNoteByIdUseCaseTest.kt
+++ app/src/test/java/com/example/up/domain/use_case/GetNoteByIdUseCaseTest.kt
@@ -1,7 +1,45 @@
 package com.example.up.domain.use_case
 
-import org.junit.jupiter.api.Assertions.*
+import com.example.up.domain.model.Note
+import com.example.up.domain.repository.NoteRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
 
 class GetNoteByIdUseCaseTest {
+2
+    private val repository = mockk<NoteRepository>()
 
+    private lateinit var useCase: GetNoteByIdUseCase
+
+    @Before
+    fun setup(){
+        useCase = GetNoteByIdUseCase(repository)
+    }
+
+    @Test
+    fun `should return note by id`() = runTest {
+        val note = Note(
+            id = 120
+        )
+
+        coEvery {
+            repository.getNoteById(120)
+        } returns note
+
+        val result = useCase(120)
+
+        assertEquals(
+            note,
+            result
+        )
+
+        coVerify {
+            repository.getNoteById(120)
+        }
+    }
 }
